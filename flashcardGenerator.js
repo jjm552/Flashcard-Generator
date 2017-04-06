b9f1941a41788ea63df00c9aea444e94f98434cf
Jeff <mcmahan.jeff@gmail.com>
2017-04-05 22:32:15 -0500

    basic and clozen constructors started

---
+++ flashcardGenerator.js
@@ -0,0 +1,13 @@
+var CardBasic = function(front, back) {
+
+    this.cardBasicFront = cardBasicFront;
+    this.cardBasicBack = cardBasicBack;
+
+};
+
+var CardClozen = function(deleted, partial, full) {
+    this.clozenDeleted = clozenDeleted;
+    this.clozenPartial = clozenPartial;
+    this.clozenFull = clozenFull;
+
+};
