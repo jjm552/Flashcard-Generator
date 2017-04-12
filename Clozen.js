fb5abf2de3ae4e04b3b4ab55666c9203702ec7c8
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-04-11 22:15:57 -0500

    basic and clozen .js removes, logic added to flashCardGenerator.js

--- Clozen.js
+++
@@ -1,8 +0,0 @@
-var CardClozen = function(deleted, partial, full) {
-    this.clozenDeleted = clozenDeleted;
-    this.clozenPartial = clozenPartial;
-    this.clozenFull = clozenFull;
-
-};
-
-module.exports = CardClozen;
